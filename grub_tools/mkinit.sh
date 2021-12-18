6f339eb7ac77013428ba59e043fd148eda90e10e
Naidanacami <68215728+naidanacami@users.noreply.github.com>
2021-12-18 14:56:23 -0700

    readd grub installer

--- grub_tools/mkinit.sh
+++ grub_tools/mkinit.sh
@@ -4,8 +4,8 @@ pacman -S --noconfirm --needed grub efibootmgr dosfstools mtools os-prober lvm2
 
 # # Edit mkinitcpio.conf for LUKS
 sed -i 's/#.*HOOKS=(/placeholder/' /etc/mkinitcpio.conf																						# Replaces all commented hooks with a placeholder so the next command won't uncomment all of them
-sed -i '/HOOKS=(/c\HOOKS=(base udev autodetect keymap modconf block keyboard encrypt lvm2 filesystems fsck)' /etc/mkinitcpio.conf			# Edit hooks
-sed -i 's/placeholder/#     HOOKS=(/' /etc/mkinitcpio.conf																				# Replace placeholder with originals
+sed -i '/HOOKS=(/c\HOOKS=(base udev autodetect keymap modconf block encrypt lvm2 filesystems keyboard fsck)' /etc/mkinitcpio.conf			# Edit hooks
+sed -i 's/placeholder/#    HOOKS=(/' /etc/mkinitcpio.conf																				# Replace placeholder with originals
 
 cat /etc/mkinitcpio.conf | grep HOOKS=
 echo "now do mkinitcpio -p linux"
