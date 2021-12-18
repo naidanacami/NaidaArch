0542a26aeee7097e846aaea77eaaf87699ab29dc
Naidanacami <68215728+naidanacami@users.noreply.github.com>
2021-12-18 15:40:02 -0700

    manual mkinitcpio

--- 1-setup.sh
+++ 1-setup.sh
@@ -133,34 +133,34 @@ if [ -e "$configFileName" ]; then
 	. $configFileName
 fi
 
-# Edit mkinitcpio.conf for LUKS																						# Edit mkinitcpio
-# TODO this is the issue. (mkinitcpio)
-sed -i 's/#.*HOOKS=(/placeholder/' /etc/mkinitcpio.conf																						# Replaces all commented hooks with a placeholder so the next command won't uncomment all of them
-sed -i '/HOOKS=(/c\HOOKS=(base udev autodetect keymap modconf block encrypt lvm2 filesystems keyboard fsck)' /etc/mkinitcpio.conf			# Edit hooks
-sed -i 's/placeholder/#    HOOKS=(/' /etc/mkinitcpio.conf																					# Replace placeholder with originals
-mkinitcpio -p linux
+# # Edit mkinitcpio.conf for LUKS																						# Edit mkinitcpio
+# # TODO this is the issue. (mkinitcpio)
+# sed -i 's/#.*HOOKS=(/placeholder/' /etc/mkinitcpio.conf																						# Replaces all commented hooks with a placeholder so the next command won't uncomment all of them
+# sed -i '/HOOKS=(/c\HOOKS=(base udev autodetect keymap modconf block encrypt lvm2 filesystems keyboard fsck)' /etc/mkinitcpio.conf			# Edit hooks
+# sed -i 's/placeholder/#    HOOKS=(/' /etc/mkinitcpio.conf																					# Replace placeholder with originals
+# mkinitcpio -p linux
 
-# Install Grub																										# Install grub
-if [[ ! -d "/sys/firmware/efi" ]]; then
-   echo "Detected BIOS"
-#    grub-install --target=i386-pc ${disk}
-	echo "-------------------------------------------------------------------------"
-	echo "--                BIOS system not currently supported!                 --"
-	echo "--                            End of script                            --"
-	echo "-------------------------------------------------------------------------"
-	exit 0
-fi
-if [[ -d "/sys/firmware/efi" ]]; then
-   echo "Detected EFI"
-   grub-install --target=x86_64-efi --efi-directory=/boot
-fi
+# # Install Grub																										# Install grub
+# if [[ ! -d "/sys/firmware/efi" ]]; then
+#    echo "Detected BIOS"
+# #    grub-install --target=i386-pc ${disk}
+# 	echo "-------------------------------------------------------------------------"
+# 	echo "--                BIOS system not currently supported!                 --"
+# 	echo "--                            End of script                            --"
+# 	echo "-------------------------------------------------------------------------"
+# 	exit 0
+# fi
+# if [[ -d "/sys/firmware/efi" ]]; then
+#    echo "Detected EFI"
+#    grub-install --target=x86_64-efi --efi-directory=/boot
+# fi
 
-# This assumes that partition 2 is the LVM partition. It should be if the disk is zapped and properly parted.
-# edits /etc/default/grub																							# edits cfg
-lvmuuid=$(blkid -s UUID -o value /dev/sda2)
-DefaultGrub="GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=${lvmuuid}:cryptLVM root=/dev/vg1/root\""	
-python3 /root/NaidaArch/Replace_Line.py -r GRUB_CMDLINE_LINUX= -d /etc/default/grub -i "${DefaultGrub}"
-grub-mkconfig -o /boot/grub/grub.cfg
+# # This assumes that partition 2 is the LVM partition. It should be if the disk is zapped and properly parted.
+# # edits /etc/default/grub																							# edits cfg
+# lvmuuid=$(blkid -s UUID -o value /dev/sda2)
+# DefaultGrub="GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=${lvmuuid}:cryptLVM root=/dev/vg1/root\""	
+# python3 /root/NaidaArch/Replace_Line.py -r GRUB_CMDLINE_LINUX= -d /etc/default/grub -i "${DefaultGrub}"
+# grub-mkconfig -o /boot/grub/grub.cfg
 # #GRUB has been flaky...moving to chroot...BE SURE TO INSTALL GRUB IF YOU MOVE BACK
 
 # read -p "Post Grub Install Pause" pause
---
+++ grub_tools/grubinstall.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+mkinitcpio -p linux
+
+# Install Grub																										# Install grub
+if [[ ! -d "/sys/firmware/efi" ]]; then
+   echo "Detected BIOS"
+#    grub-install --target=i386-pc ${disk}
+	echo "-------------------------------------------------------------------------"
+	echo "--                BIOS system not currently supported!                 --"
+	echo "--                            End of script                            --"
+	echo "-------------------------------------------------------------------------"
+	exit 0
+fi
+if [[ -d "/sys/firmware/efi" ]]; then
+   echo "Detected EFI"
+   grub-install --target=x86_64-efi --efi-directory=/boot
+fi
+
+# This assumes that partition 2 is the LVM partition. It should be if the disk is zapped and properly parted.
+# edits /etc/default/grub																							# edits cfg
+lvmuuid=$(blkid -s UUID -o value /dev/sda2)
+DefaultGrub="GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=${lvmuuid}:cryptLVM root=/dev/vg1/root\""	
+python3 /root/NaidaArch/Replace_Line.py -r GRUB_CMDLINE_LINUX= -d /etc/default/grub -i "${DefaultGrub}"
+grub-mkconfig -o /boot/grub/grub.cfg
+#GRUB has been flaky...moving to chroot...
