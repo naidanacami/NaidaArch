b7f0f3dff4e0615ca50d58b3a7dab471334f56be
Naidanacami <68215728+naidanacami@users.noreply.github.com>
2021-12-18 21:19:06 -0700

    grub edits

--- 1-setup.sh
+++ 1-setup.sh
@@ -169,7 +169,8 @@ echo "-------------------------------------------------------------------------"
 echo "--                       Installing Packages                           --"
 echo "-------------------------------------------------------------------------"
 
-sudo pacman -S --noconfirm --needed - < ${HOME}/NaidaArch/pkg-files/pacman-pkgs.txt
+# sudo pacman -S --noconfirm --needed - < ${HOME}/NaidaArch/pkg-files/pacman-pkgs.txt
+sudo pacman -S --noconfirm --needed $(cat${HOME}/NaidaArch/pkg-files/pacman-pkgs.txt)
 
 
 echo "-------------------------------------------------------------------------"
