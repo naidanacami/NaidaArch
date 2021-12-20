774cd302151a48ae2c760c8a9c327054d21b0732
Naidanacami <68215728+naidanacami@users.noreply.github.com>
2021-12-20 15:53:02 -0700

    noconfirm added

--- Rice/lightdm_enable.sh
+++ Rice/lightdm_enable.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-sudo pacman -S lightdm python-gobject python-pyqt5 python-pyqt5-webengine python-xlib python-ruamel-yaml qt5-webengine gobject-introspection        # Web-greeter dependancies
-sudo pacman -S rsync zip make pyrcc5 
+sudo pacman -S lightdm python-gobject python-pyqt5 python-pyqt5-webengine python-xlib python-ruamel-yaml qt5-webengine gobject-introspection --noconfirm --needed        # Web-greeter dependancies
+sudo pacman -S rsync zip make pyrcc5 --noconfirm --needed
 cd ~
 git clone https://github.com/JezerM/web-greeter.git
 cd ~/web-greeter
