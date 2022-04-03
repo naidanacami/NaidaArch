a624de1773526b04a4018d4650e6810b2ea80998
Naidanacami <68215728+naidanacami@users.noreply.github.com>
2022-04-02 20:41:53 -0600

    Added cursor

--- Rice/i3-gaps.sh
+++ Rice/i3-gaps.sh
@@ -94,7 +94,7 @@ chmod +x ~/.config/rofi/custom_themes/window.sh
 
 
 # Lock
-yay -S betterlockscreen --noconfirm
+yay -S betterlockscreen --needed --noconfirm
 mkdir ~/.config/betterlockscreen
 cp $script_dir/dotfiles/betterlockscreen/run.sh ~/.config/betterlockscreen/
 chmod +x ~/.config/betterlockscreen/run.sh
@@ -105,4 +105,40 @@ sudo pacman -S redshift --needed --noconfirm
 
 
 # Compositor
-sudo pacman -S picom
+sudo pacman -S picom --needed --noconfirm
+
+
+# Theming
+yay -S phinger-cursors --needed --noconfirm
+sudo pacman -S lxappearance --needed --noconfirm
+# ------------- XDG specification -------------
+if [ ! -d "~/.icons/default/" ]; then
+    mkdir -p ~/.icons/default/
+fi
+
+if [ ! -f "~/.icons/default/index.theme" ]; then
+    touch ~/.icons/default/index.theme
+    cat <<EOF >~/.icons/default/index.theme
+[icon theme] 
+Inherits=phinger-cursors
+EOF
+else
+    cp ~/.icons/default/index.theme ~/.icons/default/index.theme.old
+    sed -i '/Inherits=/c\Inherits=phinger-cursors' ~/.icons/default/index.theme 
+fi
+
+# ------------- GTK -------------
+if [ ! -d "~/.config/gtk-3.0/" ]; then
+    mkdir -p ~/.config/gtk-3.0/
+fi
+
+if [ ! -f "~/.config/gtk-3.0/settings.ini" ]; then
+    touch ~/.config/gtk-3.0/settings.ini
+    cat <<EOF >~/.config/gtk-3.0/settings.ini
+[Settings]
+gtk-cursor-theme-name=phinger-cursors
+EOF
+else
+    cp ~/.icons/default/index.theme ~/.icons/default/index.theme.old
+    sed -i '/gtk-cursor-theme-name=/c\gtk-cursor-theme-name=phinger-cursors' ~/.icons/default/index.theme 
+fi
