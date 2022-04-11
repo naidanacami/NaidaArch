ed86589c2533bb8ef9e16b7f22bac2ad7f6bad2e
Naidanacami <68215728+naidanacami@users.noreply.github.com>
2022-04-10 18:39:43 -0600

    switch to lightdm from ly

--- Rice/display_manager.sh
+++ Rice/display_manager.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
-yay -S ly --noconfirm
-sudo systemctl enable ly.service
+# yay -S ly --noconfirm
+# sudo systemctl enable ly.service
+
+pacman -S --needed --noconfirm lightdm lightdm-webkit2-greeter lightdm-webkit-theme-litarvan
+sed -i '/greeter-session=/c\greeter-session=lightdm-webkit2-greeter' /etc/lightdm/lightdm.conf
+sed -i '/# webkit-theme/# webk_tmp_it-theme' /etc/lightdm/lightdm-webkit2-greeter.conf
+sed -i '/webkit-theme/c\webkit-theme=litarvan' /etc/lightdm/lightdm-webkit2-greeter.conf
+sed -i '/# webk_tmp_it-theme/# webkit-theme' /etc/lightdm/lightdm-webkit2-greeter.conf
+
+sudo systemctl enable lightdm.service
